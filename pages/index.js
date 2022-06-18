31dd1d00acec02bc5cfe5f521ad6d36aa24be3b2
yogesh130 <yogesh@resynctech.com>
2022-06-19 00:38:31 +0800

    Added Search bar and notifcations bell to top bar

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import { Button } from "@mantine/core";
 import Navbar from "../src/components/Navbar/Navbar";
 export default function Home() {
   return (
