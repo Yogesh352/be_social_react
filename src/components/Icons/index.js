b7820d8cede8ca7a88dd2294cf144bef3f334200
yogesh130 <yogesh@resynctech.com>
2022-06-18 12:53:03 +0800

    Added a NavBar and downloaded Icons

---
+++ src/components/Icons/index.js
@@ -0,0 +1,46 @@
+import { BsLayers } from 'react-icons/bs'
+import {
+  FiArrowDownRight,
+  FiArrowUpRight,
+  FiCalendar,
+  FiChevronLeft,
+  FiChevronRight,
+  FiUpload,
+  FiX
+} from 'react-icons/fi'
+import { GiMagnifyingGlass } from 'react-icons/gi'
+import {
+  HiOutlineBell,
+  HiOutlineFilter,
+  HiOutlineLogout,
+  HiOutlinePencil,
+  HiOutlinePlusCircle,
+  HiOutlineQuestionMarkCircle,
+  HiOutlineTemplate,
+  HiOutlineUserGroup,
+  HiOutlineUsers,
+  HiOutlineXCircle,
+  HiChat, 
+  HiHome
+} from 'react-icons/hi'
+
+export const DashboardIcon = HiOutlineTemplate
+export const UserIcon = HiOutlineUserGroup
+export const UsersIcon = HiOutlineUsers
+export const NotificationIcon = HiOutlineBell
+export const CrossIcon = FiX
+export const FilterIcon = HiOutlineFilter
+export const AdditionIcon = HiOutlinePlusCircle
+export const EditIcon = HiOutlinePencil
+export const DeleteIcon = HiOutlineXCircle
+export const LogoutIcon = HiOutlineLogout
+export const UploadIcon = FiUpload
+export const PositiveChangeIcon = FiArrowUpRight
+export const NegativeChangeIcon = FiArrowDownRight
+export const QuestionMarkIcon = HiOutlineQuestionMarkCircle
+export const ErrorIcon = GiMagnifyingGlass
+export const CalendarIcon = FiCalendar
+export const LeftIcon = FiChevronLeft
+export const RightIcon = FiChevronRight
+export const ChatIcon = HiChat
+export const HomeIcon = HiHome
